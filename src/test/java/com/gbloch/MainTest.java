1c2daffcd16d172fb802b609275772a618ddc088
gbloch <gaetan.bloch@gmail.com>
2021-02-28 18:20:41 +0100

    Using profiles to set up different environment variable value

--- src/test/java/com/gbloch/MainTest.java
+++ src/test/java/com/gbloch/MainTest.java
@@ -1,11 +1,12 @@
 package com.gbloch;
 
-public class MainTest {
-    public void test1() {
-        System.out.println("--- TEST 1 ---");
-    }
+import org.junit.jupiter.api.Test;
 
-    public void test2() {
-        System.out.println("--- TEST 2 ---");
+public class MainTest {
+    @Test
+    public void test1() {
+        System.out.println("##############################");
+        System.out.println(System.getenv("TEST_HOST"));
+        System.out.println("##############################");
     }
 }
